315b86df9d8a7df23847838ae067698247bdd8ec
sky <hsf1002@gmail.com>
2019-06-09 17:37:30 +0800

    7-3

---
+++ code/7-进程环境/73.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+static void my_exit1(void);
+static void my_exit2(void);
+
+/*
+    atexit: 退出注册函数 
+*/
+int main(int argc, char *argv[])
+{
+    if (atexit(my_exit2) != 0)
+        ferror("register my_exit2 failed ");
+    if (atexit(my_exit1) != 0)
+        ferror("register my_exit1 failed ");
+    if (atexit(my_exit1) != 0)
+        ferror("register my_exit1 failed ");
+    
+    printf("main is done \n");
+
+    return(0);
+}
+
+static void my_exit1(void)
+{
+    printf("first exit handler \n");
+}
+
+static void my_exit2(void)
+{
+    printf("second exit handler \n");
+}
