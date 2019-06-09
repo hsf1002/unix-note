4c3fefd2a65c9ab01bec35320c343246163175ca
sky <hsf1002@gmail.com>
2019-06-09 17:29:34 +0800

    6-11

---
+++ code/6-系统数据文件和信息/611.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+/*
+    time、localtime、strftime    
+*/
+int main(int argc, char *argv[])
+{
+    time_t t;
+    struct tm *tmp;
+    char buf1[16];
+    char buf2[64];
+
+    // 获取日历时间
+    time(&t);
+    // 将日历时间转为本地时间
+    tmp = localtime(&t);
+
+    // 将格式化时间的字符串保存到buf
+    if (strftime(buf1, 16, "time and date: %r, %a %b %d, %Y", tmp) == 0)
+    {
+        printf("buffer len 16 is too small \n");
+    }
+    else
+    {
+        printf("%s\n", buf1);
+    }
+    
+    if (strftime(buf2, 64, "time and date: %r, %a %b %d, %Y", tmp) == 0)
+    {
+        printf("buffer len 64 is too small \n");
+    }
+    else
+    {
+        printf("%s\n", buf2);
+    }
+
+    return(0);
+}
+
