8b426a754843673655bed66eab53ea2e2aff2452
sky <hsf1002@gmail.com>
2020-09-28 16:07:48 +0800

    10-update

--- 10-信号/10-05.c
+++ 10-信号/10-05.c
@@ -11,7 +11,8 @@ static void my_alarm(int signo)
     printf("in signal handler \n");
     if (rootptr = getpwnam("root") == NULL)
         ferror("getpwnam(root) error ");
-    
+	else
+		printf("return value, pw_name = %s \n", rootptr->pw_name);    
     // 1秒钟执行一次my_alarm
     alarm(1);
 }
@@ -20,7 +21,14 @@ static void my_alarm(int signo)
     getpwnam是不可重入函数，主程序和信号处理程序都会调用，可能造成返回结果具有随机性
 
 ./a.out 
-Segmentation fault: 11
+段错误 (核心已转储)
+
+for里面添加sleep(1)之后：
+./a.out 
+return value, pw_name = hefeng 
+in signal handler 
+段错误 (核心已转储)
+
 
     主程序已经调用getpwnam，当free时，信号处理函数中断了它，也调用getpwnam，并且同时进行free，
     使得数据结构出现损坏，因此运行几秒就会产生SIGSEGV信号终止，主程序可能仍能正常运行，但是返回值有时正确，有时错误
@@ -34,11 +42,14 @@ int main(int argc, char **argv)
 
     for (;;)
     {
-        if ((ptr = getpwnam("sar")) == NULL)
+        if ((ptr = getpwnam("hefeng")) == NULL)
             ferror("getpwnam error");
-        
-        if (strcmp(ptr->pw_name, "sar") != 0)
-            printf("return value corrupted, pw_name = %s \n", ptr->pw_name);
+		else        
+            printf("return value, pw_name = %s \n", ptr->pw_name);
+		
+		if (strcmp(ptr->pw_name, "hefeng") != 0)
+			printf("return value corrupted!\n", ptr->pw_name);
+		sleep(1);
     }
 }
 
--- 10-信号/10-11.c
+++ 10-信号/10-11.c
@@ -18,6 +18,12 @@ sig_alarm(int signo)
 /*
     带时间限制调用read，使用longjmp
 
+./a.out 
+hello world read timeout 
+: Success
+ddddd
+hello world ddddd
+
     不管系统是否重新启动被中断的系统调用，程序都会如预期工作，但是仍然存在10-8与其他信号处理程序交互的问题
 
     如果要对IO操作设置时间限制，可以使用longjmp
@@ -32,13 +38,15 @@ int main(void)
     if (signal(SIGALRM, sig_alarm) == SIG_ERR)
         perror("signal(SIGALRM) error \n");
 
+	// 10秒内没有任何操作，则又会重复
     if (setjmp(env_alrm) != 0)
         perror("read timeout \n");
 
     // 设置10秒的定时
     alarm(10);
     
-    if ((n = read(STDIN_FILENO, line, MAXLINE)) < 0)
+    // 10秒内如果按回车，则会读取到数据，read退出
+	if ((n = read(STDIN_FILENO, line, MAXLINE)) < 0)
         perror("read error \n");
 
     // 关闭定时
